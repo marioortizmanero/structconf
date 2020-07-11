f505a00bb02d2b8c2cf0e4c64590051344f0150b
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-07-11 20:31:48 +0200

    Cleanup of tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,4 +1,7 @@
-//! Testing the overall functionality of StructConf.
+//! Testing the overall functionality of StructConf. Some of the tests may
+//! create new config files, which will appear in the
+//! `target/tests/structconf` directory. These files should have different
+//! names to avoid conflicts.
 
 use std::default::Default;
 use std::fmt;
@@ -8,77 +11,9 @@ use structconf::{StructConf, Error};
 use std::str::FromStr;
 use strum_macros::{Display, EnumString};
 
-#[derive(Debug, PartialEq, StructConf)]
-struct BasicAttributesConfig {
-    #[conf(no_file)]
-    no_file: i32,
-    #[conf(no_short)]
-    no_short: bool,
-    #[conf(no_long, short = "x")]
-    no_long: bool,
-    #[conf(no_short, no_long)]
-    no_short_no_long: bool,
-    #[conf(file = "new_file")]
-    file: bool,
-    #[conf(long = "name")]
-    long: bool,
-    #[conf(short = "s")]
-    short: bool,
-    #[conf(file = "new_combined", long = "new_combined", short = "c")]
-    combined: bool,
-}
-
-#[test]
-fn basic_attributes() {
-    let mut f = File::create("basic_attributes.ini").unwrap();
-    // `no_file` should be 0 because this option isn't available in the
-    // config file.
-    // `no_short_no_long` and others are not included; they should be false.
-    // `new_file` and `new_combined` have been renamed from `file` and
-    // `combined`, respectively.
-    f.write(b"
-    [Defaults]
-    no_file = 1234
-    no_short = \"true\"
-    no_long = true
-    new_file = true
-    new_combined = true
-    ").unwrap();
-
-    let app = clap::App::new("test");
-    let conf = BasicAttributesConfig::parse(app, "basic_attributes.ini").unwrap();
-    assert_eq!(conf.no_file, 0);
-    assert_eq!(conf.no_short, true);
-    assert_eq!(conf.no_long, true);
-    assert_eq!(conf.no_short_no_long, false);
-    assert_eq!(conf.file, true);
-    assert_eq!(conf.long, false);
-    assert_eq!(conf.short, false);
-    assert_eq!(conf.combined, true);
-
-    // Checking errors when parsing the config file
-    let mut f = File::create("basic_attributes.ini").unwrap();
-    f.write(b"
-    [Defaults]
-    no_short = \"should be a boolean\"
-    ").unwrap();
-
-    let app = clap::App::new("test");
-    match BasicAttributesConfig::parse(app, "basic_attributes.ini") {
-        Err(Error::Parse(_)) => assert!(true),
-        s => assert!(false, "parse error not returned: {:?}", s)
-    }
-
-    // Checking errors when creating the config file
-    let app = clap::App::new("test");
-    match BasicAttributesConfig::parse(app, "/") {
-        Err(Error::IO(_)) => assert!(true),
-        _ => assert!(false, "IO error not returned")
-    }
-}
-
-// The `strum` crate takes care of the automatic conversion to and from
-// a string.
+/// Defining an enum to use in a StructConf-derived structure.
+/// The `strum` crate takes care of the automatic conversion to and from
+/// a string.
 #[derive(Debug, Clone, PartialEq, Display, EnumString)]
 enum MyEnum {
     One,
@@ -92,6 +27,8 @@ impl Default for MyEnum {
     }
 }
 
+/// Defining a struct to use in a StructConf-derived structure.
+/// The required traits are implemented manually.
 #[derive(Debug, Clone, PartialEq)]
 struct MyStruct {
     data: i32,
@@ -126,22 +63,105 @@ impl Default for MyStruct {
     }
 }
 
-#[derive(StructConf)]
-struct CustomConfig {
+#[derive(Debug, PartialEq, StructConf)]
+struct Config {
+    #[conf(no_file)]
+    no_file: i32,
+    #[conf(no_short)]
+    no_short: bool,
+    #[conf(no_long, short = "x")]
+    no_long: bool,
+    #[conf(no_short, no_long)]
+    no_short_no_long: bool,
+    #[conf(file = "new_file")]
+    file: bool,
+    #[conf(long = "name")]
+    long: bool,
+    #[conf(short = "s")]
+    short: bool,
+    #[conf(file = "new_combined", long = "new_combined", short = "c")]
+    combined: bool,
+    #[conf(no_short)]
     someenum: MyEnum,
+    #[conf(no_short)]
     astruct: MyStruct,
+    #[conf(no_short)]
+    option_i32: Option<i32>,
+    #[conf(no_short)]
+    option_enum: Option<MyEnum>,
+    #[conf(no_short)]
+    option_string: Option<String>,
 }
 
 #[test]
+fn file_read() {
+    const FILE_NAME: &str = "file_read.ini";
+    let mut f = File::create(FILE_NAME).unwrap();
+    // `no_file` should be 0 because this option isn't available in the
+    // config file.
+    // `no_short_no_long` and others are not included; they should be false.
+    // `new_file` and `new_combined` have been renamed from `file` and
+    // `combined`, respectively.
+    f.write(b"
+    [Defaults]
+    no_file = 1234
+    no_short = \"true\"
+    no_long = true
+    new_file = true
+    new_combined = true
+    ").unwrap();
+
+    let app = clap::App::new("test");
+    let conf = Config::parse(app, FILE_NAME).unwrap();
+    assert_eq!(conf.no_file, 0);
+    assert_eq!(conf.no_short, true);
+    assert_eq!(conf.no_long, true);
+    assert_eq!(conf.no_short_no_long, false);
+    assert_eq!(conf.file, true);
+    assert_eq!(conf.long, false);
+    assert_eq!(conf.short, false);
+    assert_eq!(conf.combined, true);
+}
+
+/// Testing the errors that may be thrown when parsing the config.
+#[test]
+fn errors() {
+    const FILE_NAME: &str = "errors.ini";
+    // Checking errors when parsing the config file
+    let mut f = File::create(FILE_NAME).unwrap();
+    f.write(b"
+    [Defaults]
+    no_short = \"should be a boolean\"
+    ").unwrap();
+
+    let app = clap::App::new("test");
+    match Config::parse(app, FILE_NAME) {
+        Err(Error::Parse(_)) => assert!(true),
+        s => assert!(false, "parse error not returned: {:?}", s)
+    }
+
+    // Checking errors when creating the config file. The root directory
+    // should throw an IO error because it's a directory, and possibly
+    // because of invalid permissions.
+    let app = clap::App::new("test");
+    match Config::parse(app, "/") {
+        Err(Error::IO(_)) => assert!(true),
+        _ => assert!(false, "IO error not returned")
+    }
+}
+
+/// Testing parsing for custom structures.
+#[test]
 fn custom() {
+    const FILE_NAME: &str = "custom.ini";
     // Making sure the default values work.
-    let mut f = File::create("custom.ini").unwrap();
+    let mut f = File::create(FILE_NAME).unwrap();
     f.write(b"
     [Defaults]
     ").unwrap();
 
     let app = clap::App::new("test");
-    let conf = CustomConfig::parse(app, "custom.ini").unwrap();
+    let conf = Config::parse(app, FILE_NAME).unwrap();
     assert_eq!(conf.someenum, MyEnum::One);
     assert_eq!(conf.astruct, MyStruct {
         data: 0,
@@ -155,17 +175,21 @@ fn custom() {
     ").unwrap();
 
     let app = clap::App::new("test");
-    let conf = CustomConfig::parse(app, "custom.ini").unwrap();
+    let conf = Config::parse(app, FILE_NAME).unwrap();
     assert_eq!(conf.someenum, MyEnum::Two);
     assert_eq!(conf.astruct, MyStruct {
         data: 123,
         moredata: String::from("strval"),
     });
+}
 
+#[test]
+fn write_file() {
+    const FILE_NAME: &str = "write_file.ini";
     // Writing values into the file
     let app = clap::App::new("test");
-    let args = CustomConfig::parse_args(app);
-    let mut conf = CustomConfig::parse_file(&args, "custom.ini").unwrap();
+    let args = Config::parse_args(app);
+    let mut conf = Config::parse_file(&args, FILE_NAME).unwrap();
     let written_enum = MyEnum::Three;
     let written_struct = MyStruct {
         data: 999,
@@ -173,10 +197,10 @@ fn custom() {
     };
     conf.someenum = written_enum.clone();
     conf.astruct = written_struct.clone();
-    conf.write_file("custom.ini").unwrap();
+    conf.write_file(FILE_NAME).unwrap();
 
     // Reading it again to make sure the values were written correctly.
-    let conf = CustomConfig::parse_file(&args, "custom.ini").unwrap();
+    let conf = Config::parse_file(&args, FILE_NAME).unwrap();
     assert_eq!(conf.someenum, written_enum);
     assert_eq!(conf.astruct, written_struct);
 }
@@ -186,50 +210,14 @@ fn inverse_arg() {
     // TODO
 }
 
-#[derive(StructConf)]
-struct EmptyConfig {
-    #[conf(no_file, no_short, no_long)]
-    ignore_all: i32,
-}
-
 #[test]
-fn empty() {
+fn empty_field() {
     // TODO
-}
-
-#[derive(StructConf)]
-struct DefaultConfig {
-    #[conf(default = "123")]
-    int1: i32,
-    #[conf(default = "1.3")]
-    float1: f64,
-    #[conf(default = "true")]
-    bool1: bool,
-    #[conf(default = "String::from(\"name\")")]
-    str1: String,
 }
 
 #[test]
 fn default() {
     // TODO
-}
-
-#[derive(StructConf)]
-struct OptionConfig {
-    a: Option<i32>,
-    b: Option<f64>,
-    d: Option<MyEnum>,
-    e: Option<String>,
-    #[conf(no_short)]
-    f: Option<String>,
-    #[conf(no_long)]
-    g: Option<String>,
-    #[conf(no_short, no_long)]
-    h: Option<String>,
-    #[conf(no_file)]
-    i: Option<String>,
-    #[conf(no_short, no_long, no_file)]
-    j: Option<String>,
 }
 
 #[test]
